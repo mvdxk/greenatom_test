082064cee1983cb95f686e3093d47df159ca632d
STASYAY <stexovstas@gmail.com>
2022-03-30 14:27:59 +0300

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ res = {
     'usd': '',
     'eur': ''
 }
-thead = ['Значение курса промежуточного клиринга ', 'Время', 'Значение курса основного клиринга ', 'Время']
+thead = ['Дата', 'Значение курса промежуточного клиринга ', 'Время', 'Значение курса основного клиринга ', 'Время']
 
 usd = []
 eur = []
@@ -26,42 +26,51 @@ def makeDict(vals):
         dict.append(a)
     return dict
 
-def createFile(usd, eur):
+def join(usd, eur):
+    data = {}
+    el = ['-', '-', '-', '-', '-']
+    for u in usd:
+        data[u[0]] = u + el
+    for e in eur:
+        if list(data.keys()).count(e[0]) == 0:
+            data[e[0]] = el + e[1:]
+        else:
+            data[e[0]] = data[e[0]][:5] + e
+    return data
+
+def createFile(data):
     file = xl.Workbook()
     sheet = file.active
-    sheet['A1'] = 'Дата'
-    sheet.cell(row=1, column=1).alignment = Alignment(horizontal='center')
     sheet['J1'] = 'Изменение'
     sheet.cell(row=1, column=9).alignment = Alignment(horizontal='center')
-    col = 2
+    col = 1
     for c in range(len(currs)):
         for t in range(len(thead)):
             h = thead[t]
-            if col % 2 == 0:
+            if h.find('курса') != -1:
                 h += currs[list(currs.keys())[c]]
             sheet.cell(row=1, column=col, value=h)
             sheet.cell(row=1, column=col).alignment = Alignment(horizontal='center')
             col += 1
     row = 2
-    for e in eur:
-        for d in usd:
-            r = []
-            if e[0] == d[0]:
-                r = d + e[1:]
-                for i in range(9):
-                    val = r[i]
-                    if val.count(':') == 0 and val.count('.') == 1:
-                        val = float(val)
-                    sheet.cell(row=row, column=i+1, value=val)
-                    sheet.cell(row=row, column=i+1).alignment = Alignment(horizontal='center')
 
-                val = '-'
-                if e[-2] != '-' and d[-2] != '-':
-                    val = float(e[-2])/float(d[-2])
-                sheet.cell(row=row, column=10, value=val)
-                sheet.cell(row=row, column=10).alignment = Alignment(horizontal='center')
-                row += 1
-    for l in ['B', 'D', 'F',  'H']:
+    for k in data.keys():
+        d = data[k]
+        for i in range(9):
+            val = d[i]
+            if val.count(':') == 0 and val.count('.') == 1:
+                val = float(val)
+            sheet.cell(row=row, column=i + 1, value=val)
+            sheet.cell(row=row, column=i + 1).alignment = Alignment(horizontal='center')
+
+        val = '-'
+        if d[3] != '-' and d[-2] != '-':
+            val = float(d[-2]) / float(d[3])
+        sheet.cell(row=row, column=10, value=val)
+        sheet.cell(row=row, column=10).alignment = Alignment(horizontal='center')
+        row += 1
+
+    for l in ['B', 'D', 'G',  'I']:
         sheet.column_dimensions[l].width = 46
     file.save('data.xlsx')
 
@@ -71,4 +80,5 @@ res['usd'] = res['usd'].find('table', class_='tablels').find_all('tr')[2:]
 res['eur'] = res['eur'].find('table', class_='tablels').find_all('tr')[2:]
 usd = makeDict(res['usd'])
 eur = makeDict(res['eur'])
-createFile(usd, eur)
+data = join(usd, eur)
+createFile(data)
